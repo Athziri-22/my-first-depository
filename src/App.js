92eee1cfcf18c31a0703b77f1bbe8203a050c2ab
Athziri-22 <72827192+Athziri-22@users.noreply.github.com>
2021-10-18 16:50:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Code Nation</h1>
+      <h2>Start editing to see some magic in the making!</h2>
+    </div>
+  );
+}
